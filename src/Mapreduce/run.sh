ec6772d58a5dc2d5663f5f63bb89f9a34b8b68a9
narkisim <40834477+narkisim@users.noreply.github.com>
2018-07-05 14:13:12 +0300

    Create run.sh

---
+++ src/Mapreduce/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf $(pwd)/wordcount/output
+
+# declare the location array
+cities=("nyc" "boston" "miami" "austin")
+
+## now loop through the above array
+for i in "${cities[@]}"
+do
+   echo "Lookup: $i"
+   # execute the python script
+   # python ~/cloud/python/twits.py -l $i
+   python python/twits.py -l $i
+   # do the map-reduce
+   mvn exec:java -Dexec.mainClass=com.igalia.wordcount.App -Dexec.args="wordcount/input/$i/ wordcount/output/$i/"
+   # /usr/local/hadoop/bin/hadoop jar target/wordcount-0.0.1-SNAPSHOT.jar ~/wordcount/input/$i/ ~/wordcount/output/$i/
+done 
